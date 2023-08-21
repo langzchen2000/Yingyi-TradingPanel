84a2f0ff2a4d65afebd65edd75003b5cb0961762
langs <630250445@qq.com>
2023-08-21 15:16:46 +0800

    config nginx reverse proxy

--- src/OKX_SDK/market.js
+++ src/OKX_SDK/market.js
@@ -1,15 +1,12 @@
 
-const baseURL = 'https://www.okx.com'
-
-
 export const fetchMarketPrice = async function (instId) {
-    const response = await fetch(`${baseURL}/api/v5/market/ticker?instId=${instId}`);
+    const response = await fetch(`/api/v5/market/ticker?instId=${instId}`);
     const data = await response.json();
     return data.data[0].last;
 }
 
 export const fetchMarketBooks = async function (instId, size) {
-    const response = await fetch(`${baseURL}/api/v5/market/books?instId=${instId}&sz=${size}`);
+    const response = await fetch(`/api/v5/market/books?instId=${instId}&sz=${size}`);
     const data = await response.json();
     return {
         asks: data.data[0].asks,
